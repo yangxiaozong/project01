294a3382512f48a90ffe36029c4e757f82b2334d
yangxiaozong <1335341353@qq.com>
2017-05-06 19:12:41 +0800

    test方法的添加

---
+++ demo/src/demo/Demo01.java
@@ -0,0 +1,13 @@
+package demo;
+
+public class Demo01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+	}
+	public void test(){
+		System.out.println("===");
+	}
+
+}
