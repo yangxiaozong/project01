267b977e2197b38b0ed9728fe1a7a178c896acac
yangxiaozong <1335341353@qq.com>
2017-05-07 14:56:26 +0800

    删掉.

--- demo/src/demo/Demo.java
+++ demo/src/demo/Demo.java
@@ -1,11 +1,8 @@
 package demo;
 
 public class Demo {
-	public static void main(String[] args) {
-		System.out.println("git测试");
-		System.out.println("git第二次测试");
-		System.out.println("git第二次测试12121");
-		System.out.println("git第二次测试12121");
-		System.out.println("git第二次测试1212111");
+	public String getName(){
+		System.out.println("测试1234");
+		return null;
 	}
 }
